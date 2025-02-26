5e7e08cd59e0f7a5e9c28dba1a5af7814522355f
DaoChaShao <27882272@qq.com>
2025-02-26 23:14:05 +1300

    Initial commit

---
+++ utilis/tools.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/2/26 23:03
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   tools.py
+# @Desc     :
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
