256032b924063a342fe1a9ef2d2f16a75b6904fc
DaoChaShao <27882272@qq.com>
2025-02-27 20:34:20 +1300

    feat: implement main function for memory summary with API key validation and conversation handling

--- subpages/memory_summary.py
+++ subpages/memory_summary.py
@@ -6,9 +6,39 @@
 # @File     :   memory_summary.py
 # @Desc     :   
 
+from uuid import uuid4
+
+from utilis.tools import is_key, opener, histories_summary_getter, conversations_summary_getter, Timer
+
+
 def main() -> None:
     """ Main Function """
-    pass
+    if is_key():
+        print("The API key within the environment is set.")
+
+        chat = opener(temperature=0.0)
+        if chat is not None:
+            print("OpenAI API Key is valid.")
+
+            # Set up the id number of the whole conversation
+            session_id = str(uuid4())
+            # Set up the memory object
+            histories: dict = {}
+            # Set up the memory store
+            memory = histories_summary_getter(session_id, histories, chat)
+
+            # Star the conversation
+            prompts: list[str] = [
+                "Please give me 3 random numbers",
+                "Please sum the numbers",
+            ]
+            with Timer(2, "Conversation in Summary Counter", ) as timer:
+                conversations_summary_getter(chat, memory, prompts, session_id)
+            print(timer)
+        else:
+            print("OpenAI API Key is invalid.")
+    else:
+        print("The API key within the environment is not set.")
 
 
 if __name__ == "__main__":
